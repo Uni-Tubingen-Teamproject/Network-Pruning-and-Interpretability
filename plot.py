0a40c4a656e9e9b54c0c572af18aa8c1436b247f
Jonathan von Rad <jonathan.vonrad@gmail.com>
2024-04-28 19:10:54 +0200

    feat: plotted datapoints as surface for better design

--- plot.py
+++ plot.py
@@ -4,27 +4,20 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # Laden der Daten aus der results.npy-Datei
 data = np.load('results.npy')
+data2 = np.load('results (1).npy')
 
 mean_array = np.mean(data, axis=-1)
 fig = plt.figure()
-ax = fig.add_subplot( projection='3d')
-i = 0
+ax = fig.add_subplot(projection='3d')
 
+# Erstellung des X- und Y-Gitters für die Oberfläche
+X, Y = np.meshgrid(np.arange(0, 1.0, 0.1), np.arange(0, 1.0, 0.1))
 
-# Verschachtelte For-Schleife
-for x in np.arange(0, 1.0, 0.1):
-    for y in np.arange(0, 1.0, 0.1):
-        # Wandele x und y in ganzzahlige Indizes um
-        x_index = int(x * 10)
-        y_index = int(y * 10)
-        ax.scatter(x, y, mean_array[x_index][y_index])
-        # Greife auf das mean_array zu und verwende die ganzzahligen Indizes
-	
+# Plot der Oberfläche
+ax.plot_surface(X, Y, mean_array, cmap='viridis')
 
-
-
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+ax.set_xlabel('Conv 1')
+ax.set_ylabel('Conv 2')
+ax.set_zlabel('Accuracy')
 
 plt.show()
